ad4715abacc5e35b610286c01785db0d04a28ff6
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-11-09 13:41:53 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import { fetchData, page, searchParams } from "./fetchData"
+import { renderMarkup } from "./renderMarkup"
+import Notiflix from "notiflix"
+
+let page = 1;
+// const button = document.querySelector(".button")
+// const input = document.querySelector(".input")
+const form = document.querySelector(".search-form")
+const buttonLoadMore = document.querySelector(".button_load-more")
+const gallery = document.querySelector(".gallery")
+
+let request;
+let markup;
+let toRender;
+
+form.addEventListener("submit", onSubmit)
+form.addEventListener("input", onInput)
+buttonLoadMore.addEventListener("click", onLoadMore)
+
+function onInput(event) {
+    request = event.target.value;
+    return request;
+}
+
+async function onSubmit (event) {
+ 
+  event.preventDefault();
+  resetPage()
+   const response = await fetchData(request, page)
+   console.log(response);
+   let fetching = await response.data.hits
+   console.log(fetching)
+    if(fetching.length === 0) {
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."); 
+    }
+    toRender = await fetching
+    renderMarkup(toRender)
+    buttonLoadMore.classList.remove("is-hidden")
+    if(fetching.length < 40) {
+      buttonLoadMore.classList.add("is-hidden")
+    }
+}
+function resetPage() {
+  page = 1
+  gallery.innerHTML = ""
+}
+async function addPage (page) {
+  const newPage = await page + 1
+  return newPage
+}
+async function onLoadMore () {
+  const preparingForAdditionalFetch = await fetchData(request, addPage)
+  console.log(preparingForAdditionalFetch)
+  const additionalFetch = await preparingForAdditionalFetch.data.hits
+  if(additionalFetch.length < 40) {
+    buttonLoadMore.classList.add("is-hidden")
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+  console.log(additionalFetch)
+  renderMarkup(additionalFetch)
+}
+
+export {gallery, page}
---
+++ src/renderMarkup.js
@@ -0,0 +1,31 @@
+import { gallery } from ".";
+
+function renderMarkup(array) {
+    markup = array.map(item => {
+    return `<div class="photo-card">
+    <img class="image"src=${item.webformatURL} alt=${item.tags} loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${item.likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${item.views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${item.comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${item.downloads}
+      </p>
+    </div>
+    </div>`
+     }).join('')
+     gallery.innerHTML += markup;
+    }
+
+    
+export {renderMarkup}
